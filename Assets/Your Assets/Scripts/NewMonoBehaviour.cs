4b5975b4be2f95985d820a8f9493db7b35fef63f
Alfie213 <arturbogoratov@gmail.com>
2023-05-05 20:30:57 +0300

    Added some scripts

---
+++ Assets/Your Assets/Scripts/NewMonoBehaviour.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Alfie
+{
+    public class AlfieFirstInitializer : Initializer
+	{
+        public sealed override void InitializeScene()
+        {
+            
+        }
+        public sealed override void DeinitializeScene()
+        {
+
+        }
+    }
+}
