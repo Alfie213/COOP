d43a595cddf93a656b8b49342063c670ccbe6738
Alfie213 <arturbogoratov@gmail.com>
2023-05-06 20:21:50 +0300

    Destroying

--- Assets/Your Assets/Scripts/Wall.cs
+++ Assets/Your Assets/Scripts/Wall.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace Alfie
 {
 	public class Wall : MonoBehaviour
 	{
-		[SerializeField] private int _health = 10;
+		[SerializeField] private int _health = 100;
+		[SerializeField] private TextMeshProUGUI _healthTMP;
+
+		private void Awake()
+		{
+			_healthTMP.text = _health.ToString();
+		}
 
 		private void OnCollisionEnter(Collision collision)
 		{
@@ -14,9 +21,12 @@ namespace Alfie
             {
                 Debug.Log("Hit");
                 _health--;
+				_healthTMP.text = _health.ToString();
 				if (_health <= 0)
 				{
-					Destroy(this);
+					_healthTMP.text = string.Empty;
+					Debug.Log("all");
+					Destroy(gameObject);
 				}
             }
         }
